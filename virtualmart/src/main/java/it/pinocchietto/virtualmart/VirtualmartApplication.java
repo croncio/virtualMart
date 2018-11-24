0239e23c1f7f7d063a53eba6ed33292e41f6b5ab
IanoBoss <45310843+IanoBoss@users.noreply.github.com>
2018-11-24 13:03:41 +0100

    base
    Povero roncino

---
+++ virtualmart/src/main/java/it/pinocchietto/virtualmart/VirtualmartApplication.java
@@ -0,0 +1,12 @@
+package it.pinocchietto.virtualmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualmartApplication.class, args);
+	}
+}
