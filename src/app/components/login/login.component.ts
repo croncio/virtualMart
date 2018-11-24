93b8e990296c063e3e6f4d52ce6d7e8ce7794b86
Simone Guarini <csrm832@consoft.it>
2018-11-24 11:53:36 +0100

    primo commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../service/auth-service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userModel: User;
+  errorMessage: string;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.errorMessage = '';
+    this.userModel = new User(0, '', '', '', '', []);
+    localStorage.removeItem('token');
+    localStorage.removeItem('currentUser');
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.blockUI.start();
+    this.errorMessage = '';
+    this.authService.logIn(this.userModel)
+      .subscribe(data => {
+          this.blockUI.stop();
+          this.router.navigate(['/home']);
+        }, err => {
+          this.errorMessage = 'Attenzione :  Username e/o password non sono corretti';
+          this.blockUI.stop();
+        }
+      );
+  }
+
+}
