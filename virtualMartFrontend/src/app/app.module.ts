5df98cc9022b954bf992bc72dc52a0aa4ca50d4b
Simone Guarini <csrm832@consoft.it>
2018-11-25 11:17:06 +0100

    Roncio macellaio

--- virtualMartFrontend/src/app/app.module.ts
+++ virtualMartFrontend/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { routing } from './app.routing';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {AgmCoreModule, GoogleMapsAPIWrapper} from '@agm/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {routing} from './app.routing';
 
 @NgModule({
   declarations: [
